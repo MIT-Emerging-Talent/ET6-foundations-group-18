6969b5e0e56858d66760a95a0fc1410c23aace4e
Raghad Abdul Rahim <raghadsalah526@gmail.com>
2025-01-08 12:06:48 +0300

    Update test_area_of_circle.py

--- solutions/tests/test_area_of_circle.py
+++ solutions/tests/test_area_of_circle.py
@@ -12,17 +12,18 @@ import unittest
 import math
 from solutions.area_of_circle import calculate_circle_area
 
-class TestCircleArea(unittest.TestCase):
 
+class TestCircleArea(unittest.TestCase):
     def test_positive_radius(self):
         self.assertAlmostEqual(calculate_circle_area(5), math.pi * 5**2)
         self.assertAlmostEqual(calculate_circle_area(10), math.pi * 10**2)
-        
+
     def test_zero_radius(self):
         self.assertEqual(calculate_circle_area(0), 0)
-        
+
     def test_large_radius(self):
         self.assertAlmostEqual(calculate_circle_area(1000), math.pi * 1000**2)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
